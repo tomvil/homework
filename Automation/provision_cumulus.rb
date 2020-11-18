fe369b0de3da5a3456e3af6e91d54e8d22b6a9d9
tomasv <tomas.vilemaitis@balt.net>
2020-11-18 14:26:35 +0200

    restart frr service after changing daemons file

--- Automation/provision_cumulus.rb
+++ Automation/provision_cumulus.rb
@@ -4,6 +4,7 @@ Vagrant.configure("2") do |config|
   config.vm.provision :shell, inline: "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 && apt-get update -y && apt-get install ansible -y"
   config.vm.provision :shell, inline: 'sed -i s/zebra=no/zebra=yes/ /etc/frr/daemons'
   config.vm.provision :shell, inline: 'sed -i s/bgpd=no/bgpd=yes/ /etc/frr/daemons'
+  config.vm.provision :shell, inline: 'systemctl restart frr.service'
   config.vm.network "private_network", virtualbox__intnet: "intnet-1", auto_config: false
   config.vm.network "private_network", virtualbox__intnet: "intnet-2", auto_config: false
 end
