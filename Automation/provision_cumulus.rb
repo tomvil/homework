dd91c71de6b4dde742c6e9e12ee3e006968481c4
tomasv <tomas.vilemaitis@balt.net>
2020-11-18 11:40:25 +0200

    added bats and made some small changes

--- Automation/provision_cumulus.rb
+++ Automation/provision_cumulus.rb
@@ -2,6 +2,8 @@ Vagrant.configure("2") do |config|
   config.vm.provision :shell, inline: "sudo -i"
   config.vm.provision :shell, inline: "echo -e 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main\ndeb http://deb.debian.org/debian buster main' >> /etc/apt/sources.list"
   config.vm.provision :shell, inline: "apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 && apt-get update -y && apt-get install ansible -y"
+  config.vm.provision :shell, inline: 'sed -i s/zebra=no/zebra=yes/ /etc/frr/daemons'
+  config.vm.provision :shell, inline: 'sed -i s/bgpd=no/bgpd=yes/ /etc/frr/daemons'
   config.vm.network "private_network", virtualbox__intnet: "intnet-1", auto_config: false
   config.vm.network "private_network", virtualbox__intnet: "intnet-2", auto_config: false
 end
